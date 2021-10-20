52fafeb73f4677434f26800a8a2c637fb063856a
Joseph Howard <joe.howard@mac.com>
2021-10-20 06:14:40 -0400

    Get some data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+      title: 'At the Forest',
+      url: 'https://media.istockphoto.com/photos/sun-shining-in-a-forest-picture-id901134626?b=1&k=20&m=901134626&s=170667a&w=0&h=SvvOoqv-fAHFKoX3-etEUtBXWbSh0oLYauE7B1_oycY=',
+    },
+    {
+      title: 'At the City',
+      url: 'https://media.istockphoto.com/photos/tokyo-cityscape-aerial-view-picture-id1278304139?b=1&k=20&m=1278304139&s=170667a&w=0&h=Zj3AqlA67R9saNsuJGXHuj6ROqSn0gc_mtiVbd5BBAo=',
+    },
+    {
+      title: 'At the Snow',
+      url: 'https://media.istockphoto.com/photos/christmas-snow-background-with-snow-drifts-and-snowcovered-blur-picture-id1281448663?b=1&k=20&m=1281448663&s=170667a&w=0&h=zy-4qIcna_HqubyST-uiH4GnbxcBXz9TrqviZwbHcDw=',
+    },
+  ];
 }
